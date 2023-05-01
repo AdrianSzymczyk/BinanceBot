293f8ebbf21cd021a74da3d941664e42fff739c9
AdrianSzymczyk <adiszymczyk@gmail.com>
2023-05-01 11:11:25 +0200

    File with api keys removed

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,2 @@
-
-
-binance_api: str = 'iIvUdxkGf0OlPETUQmwNBuPFtKqXN85NtDedWoxHGLiXWb3ffkpOHoaDGfJJffjU'
-secret_key: str = 'FbG8g4JwY5mUEOTWtn7Pm5bk5UR0XxUoVWC3OrXNQzx6DNhGoPG8WUillgC7cKAD'
+binance_api = 'iIvUdxkGf0OlPETUQmwNBuPFtKqXN85NtDedWoxHGLiXWb3ffkpOHoaDGfJJffjU'
+secret_key = 'FbG8g4JwY5mUEOTWtn7Pm5bk5UR0XxUoVWC3OrXNQzx6DNhGoPG8WUillgC7cKAD'
